5134734dc993be435a707766d0f4c22b871446bf
dbesar <dededarirahmadi@gmail.com>
2022-03-26 03:30:19 +0700

    success show image(real) from camera

--- app/src/main/java/com/example/fileplayground/MainActivity.kt
+++ app/src/main/java/com/example/fileplayground/MainActivity.kt
@@ -1,5 +1,6 @@
 package com.example.fileplayground
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import com.example.fileplayground.databinding.ActivityMainBinding
@@ -13,5 +14,9 @@ class MainActivity : AppCompatActivity() {
         binding = ActivityMainBinding.inflate(layoutInflater)
         val view = binding.root
         setContentView(view)
+
+        binding.showImage.setOnClickListener {
+            startActivity(Intent(this, ShowImageActivity::class.java))
+        }
     }
 }
--- app/src/main/java/com/example/fileplayground/ShowImageActivity.kt
+++ app/src/main/java/com/example/fileplayground/ShowImageActivity.kt
@@ -1,12 +1,31 @@
 package com.example.fileplayground
 
+import android.Manifest
+import android.content.pm.PackageManager
+import android.graphics.Bitmap
+import android.net.Uri
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.os.Environment
+import android.util.Log
+import androidx.activity.result.contract.ActivityResultContracts
+import androidx.activity.result.launch
+import androidx.core.app.ActivityCompat
+import androidx.core.content.FileProvider
 import com.example.fileplayground.databinding.ActivityShowImageBinding
+import java.io.File
 
 class ShowImageActivity : AppCompatActivity() {
 
+    companion object{
+        const val REQUEST_CODE = 101
+    }
+
+    private val TAG = "SHOWIMAGEACTIVITY"
+
     private lateinit var binding: ActivityShowImageBinding
+
+    private lateinit var imageUri: Uri
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -14,5 +33,60 @@ class ShowImageActivity : AppCompatActivity() {
         binding = ActivityShowImageBinding.inflate(layoutInflater)
         val view = binding.root
         setContentView(view)
+
+        requestPermissions()
+
+        val takePicture = registerForActivityResult(ActivityResultContracts.TakePicture()){
+            if (it){
+                binding.imageView.setImageURI(imageUri)
+            }
+        }
+
+        binding.btnCamera.setOnClickListener {
+
+            val photoFile = File.createTempFile(
+                "IMG_",
+                ".jpg",
+                getExternalFilesDir(Environment.DIRECTORY_PICTURES)
+            )
+
+            imageUri = FileProvider.getUriForFile(
+                this,
+                "${packageName}.provider",
+                photoFile
+            )
+
+            takePicture.launch(imageUri)
+        }
+
+    }
+
+    private fun hasCameraPermission(): Boolean {
+        return ActivityCompat.checkSelfPermission(this, android.Manifest.permission.CAMERA) == PackageManager.PERMISSION_GRANTED
+    }
+
+    private fun requestPermissions(){
+        var permissionToRequest = mutableListOf<String>()
+        if (!hasCameraPermission()){
+            permissionToRequest.add(Manifest.permission.CAMERA)
+        }
+        if (permissionToRequest.isNotEmpty()){
+            ActivityCompat.requestPermissions(this, permissionToRequest.toTypedArray(), REQUEST_CODE)
+        }
+    }
+
+    override fun onRequestPermissionsResult(
+        requestCode: Int,
+        permissions: Array<out String>,
+        grantResults: IntArray
+    ) {
+        super.onRequestPermissionsResult(requestCode, permissions, grantResults)
+        if (REQUEST_CODE == 101 && grantResults.isNotEmpty()){
+            for (i in grantResults.indices){
+                if (grantResults[i] == PackageManager.PERMISSION_GRANTED){
+                    Log.d(TAG, "${permissions[i]} granted..")
+                }
+            }
+        }
     }
 }
