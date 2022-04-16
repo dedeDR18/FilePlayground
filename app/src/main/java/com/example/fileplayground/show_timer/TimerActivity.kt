9adc63b19290831f59d04aea5ab1b3a787ec4304
dbesar <dededarirahmadi@gmail.com>
2022-04-17 02:59:11 +0700

    success run timer with fix duration

--- app/src/main/java/com/example/fileplayground/show_timer/TimerActivity.kt
+++ app/src/main/java/com/example/fileplayground/show_timer/TimerActivity.kt
@@ -2,12 +2,17 @@ package com.example.fileplayground.show_timer
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.os.CountDownTimer
 import com.example.fileplayground.R
 import com.example.fileplayground.databinding.ActivityTimerBinding
+import java.util.*
+import java.util.concurrent.TimeUnit
 
 class TimerActivity : AppCompatActivity() {
 
     private lateinit var binding: ActivityTimerBinding
+
+    private val duration: Long = 12000
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -15,5 +20,40 @@ class TimerActivity : AppCompatActivity() {
         binding = ActivityTimerBinding.inflate(layoutInflater)
         val view = binding.root
         setContentView(view)
+        countDown()
+    }
+
+    private fun countDown() {
+        binding.btnStart.setOnClickListener {
+            val cd = (object : CountDownTimer(duration * 1000, 1000) {
+                override fun onTick(milisUntilFinished: Long) {
+
+                    val time = String.format(
+                        Locale.getDefault(),
+                        "%02d:%02d:%02d",
+                        TimeUnit.MILLISECONDS.toHours(milisUntilFinished),
+                        TimeUnit.MILLISECONDS.toMinutes(milisUntilFinished) -
+                                TimeUnit.HOURS.toMinutes(
+                                    TimeUnit.MILLISECONDS.toHours(
+                                        milisUntilFinished
+                                    )
+                                ),
+                        TimeUnit.MILLISECONDS.toSeconds(milisUntilFinished) -
+                                TimeUnit.MINUTES.toSeconds(
+                                    TimeUnit.MILLISECONDS.toMinutes(
+                                        milisUntilFinished
+                                    )
+                                )
+                    )
+                    binding.countdown.text = time
+                }
+
+                override fun onFinish() {
+                    binding.countdown.text = resources.getString(R.string.done)
+                }
+
+            })
+            cd.start()
+        }
     }
 }
